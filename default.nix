1fb5622a9b9b63d14bb214da8154a4ccfb6056f4
Samuel W. Flint <swflint@flintfam.org>
2020-02-05 21:28:58 -0600

    Update building to use builder.sh

--- default.nix
+++ default.nix
@@ -4,13 +4,9 @@ nixpkgs.stdenv.mkDerivation rec {
   name = "cookbook";
   src =  ./.;
   buildInputs = [ nixpkgs.texlive.combined.scheme-full ];
-  buildPhase = ''
-export TEXINPUTS="./texmf/:$\{TEXINPUTS\}"
-
-lualatex document
-makeglossaries document
-makeindex document
-lualatex document
-lualatex document
+  buildPhase = ./builder.sh;
+  installPhase = ''
+  mkdir $out
+  cp document.pdf $out/
 '';
 }
